9d5b66a7f9680d502609322de0f90d6c338bd6ab
RutenisB <49234142+RutenisB@users.noreply.github.com>
2025-06-06 09:09:39 +0300

    Update Krituliai_Daugailiai_menesis.py

--- Krituliai_Daugailiai_menesis.py
+++ Krituliai_Daugailiai_menesis.py
@@ -55,12 +55,12 @@ bars = ax.bar(daily["date"].astype(str), daily["intensity"], color='skyblue')
 ax.set_title(f"Kritulių kiekis per {today.strftime('%B')} mėnesį")
 ax.set_xlabel("Diena")
 ax.set_ylabel("Krituliai (mm)")
-plt.xticks(rotation=0)
+plt.xticks(rotation=80)
 
 # Pridedam stulpelių reikšmes virš jų
 for bar in bars:
     height = bar.get_height()
     ax.text(bar.get_x() + bar.get_width()/2, height, f'{height:.2f}', 
-            ha='center', va='bottom', fontsize=8, rotation=90)
+            ha='center', va='bottom', fontsize=8, rotation=0)
 
 st.pyplot(fig)
